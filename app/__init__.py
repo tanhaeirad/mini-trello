3ea46b1261c8125e1d32b8c18547e8ae33f7ef08
tanhaeirad <amirrezatanhaei@yahoo.com>
2023-12-28 01:04:46 +0100

    Add CORS in backend

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from graphql_server.flask import GraphQLView
 from .graphql.schema import my_schema
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, resources={r"/graphql": {"origins": "http://localhost:5173"}})
 
     app.add_url_rule(
         "/graphql",
